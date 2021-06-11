ffa1f103ee86dc81cf88b07c920126980e3031b7
Edvan Matos <edvan.stt02@gmail.com>
2021-06-11 00:10:51 -0300

    feat: fontes e cores adicionados

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,13 +5,18 @@ export default class MyDocument extends Document {
             <Html lang="pt-BR">
                 <Head>
                     <meta charSet="utf-8" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
+                        rel="stylesheet"
+                    />
                     {/* <link
                         rel="shortcut icon"
                         href="favicon.png"
                         type="image/png"
                     />
 
-                    {/* <title>Mova-se</title> */}
+                    {/* <title>Covid-Lagarto</title> */}
                 </Head>
                 <body>
                     <Main />
